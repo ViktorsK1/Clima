725533d1a149dca8e6ee3f78340f0db7acdc30bd
Viktor Kulia <65856881+ViktorsK1@users.noreply.github.com>
2021-01-07 21:37:26 +0200

    Add files via upload

---
+++ WeatherViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Angela Yu on 01/09/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+//
+//1. change image when changing weather(to add from Internet)
+//2. change cityLable
+//3. after adding city in searchLable
+//4. click Button
+//5. to switch degree (C)
+//6. update weather View
+//7. change cityLable automaticaly
+//8. click GPS Button and update local weather
+//9. change background when black theme
+
+//change in code city and update degree, image, city name.
+//strangth update weather
